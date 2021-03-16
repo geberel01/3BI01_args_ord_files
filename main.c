7f5facf153ad420025177606ae1482eeee342bc8
Diaci <davide.iaci@icloud.com>
2021-03-16 07:15:40 +0000

    temporaneo

--- main.c
+++ main.c
@@ -1,34 +1,48 @@
-//ESERCIZIO:
-//DARE IN INGRESSO IL NOME DI UN FILE TXT
-//CON STRINGHE NUMERICHE E DI CARATTERI
-//E IL NOME DI UN FILE DI TESTO DA CREARE
-//CON TUTTE LE STRINGHE NUMERICHE
-//CONVERTITE IN BINARIO
-//E LE STRINGHE DI CARATTERI
-//IN ORDINE CRESCENTE
-
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  for(i=0;i<argc;i++)
+/* ESERCIZIO:
+DARE IN INGRESSO IL NOME DI UN FILE TXT
+CON STRINGHE NUMERICHE E DI CARATTERI
+E IL NOME DI UN FILE DI TESTO DA CREARE
+CON TUTTE LE STRINGHE NUMERICHE
+CONVERTITE IN BINARIO
+E LE STRINGHE DI CARATTERI
+IN ORDINE CRESCENTE */
+
+int main(int argc, char const *argv[])
+{
+    FILE *streamIn;        // dichiarazione dello stream
+    int numero; // numero temporaneo che poi viene convertito in binario
+    char stringa; // carattere temporaneo per stringa
+    
+    // controlla che vengano dati 2 argomenti 
+    // if (argv[3] != ' ')
+    //     printf("Inserire solo 2 argomenti");
+
+    // apertura del file
+    streamIn = fopen(argv[1], "r");
+
+    // controlla se il file viene aperto
+    if (streamIn == NULL)
     {
-      printf("Pos %d\t stringa: %s\n",i,argv[i]);
+        printf("Apertira del file %s non riuscita", argv[1]);
+        exit(1);
     }
-    printf("somma: %d\n",sum);
-  return 0;
-  strcp
+
+    // operazioni di lettura del file
+    while (!feof(streamIn))
+    {
+        fscanf(streamIn, "%d", &numero);
+        stringa = getc(streamIn);
+        printf("%d\n", numero);
+    }
+
+    
+
+    // chiusura dello stream
+    fclose(streamIn);
+
+    return 0;
 }
-
-ES. PROT. ORDINAMENTO:
-for(i=0)
-  for(j=1+1)
-   if arr[j]<arr[i] -> swap;
-   
-ES. PROT. SWAP:
-    swap(char primo[20], char secondo[20])
-    {
-      char temp[20];
-      strcpy(primo,temp);
-      strcpy(secondo,primo);
-      strcpy(temp,secondo);
-    }
